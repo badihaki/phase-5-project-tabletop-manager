b88f9cddcb2616ae624ff53520403543eca5cca6
Badi-Haki Martin <jojackblack@gmail.com>
2022-11-30 01:18:34 -0500

    set up functions needed in controllers (with pseudocode). Created User Context

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     
     def create
+        # use this for signing up
         # create the user using user_params
         # check if the user is valid
         # if valid:
