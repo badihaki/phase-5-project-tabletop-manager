a1d8e09d64265bf17a4f025f4d4dc1958421550a
Badi-Haki Martin <jojackblack@gmail.com>
2022-12-03 13:49:22 -0500

    added email validator ruby gem, set up user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
     # Associations
     has_many :memberships
     has_many :groups, through: :memberships
+
+    # validations
+    validates :name, presence: true, length: { in: 2..10 }
+    validates :password, presence: true, length: { in: 6..12 }, confirmation: true
+    validates :email, presence: true, uniqueness: true
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :memberships
   resources :groups
-  resources :users
+  resources :users, except: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
