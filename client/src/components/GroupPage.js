474df7601ff2716883d71c347e448731ad9184a6
Badi-Haki Martin <jojackblack@gmail.com>
2022-12-14 01:26:29 -0500

    created group membership form

--- client/src/components/GroupPage.js
+++ client/src/components/GroupPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { GroupsContext } from "./context components/GroupsContext";
+import JoinGroupForm from "./JoinGroupForm";
 
 function GroupPage(){
     const { groupId } = useParams();
@@ -11,8 +12,34 @@ function GroupPage(){
     return(
         <div>
             <h2>{group.name}</h2>
+            <h4>Game Master: {group.game_master.name}</h4>
             <br />
-            <Link to={"/groups"}>Back</Link>
+            <p>
+                We play {group.game} on {group.game_day}.
+                <br />
+                <br />
+                <h4>Members:</h4>
+                <ul>
+                    {group.players.map(player=>{
+                        return(
+                            <li key={player.id}>
+                                {player.name}
+                            </li>
+                        )
+                    })}
+                </ul>
+                <br />
+                <br />
+                <p>
+                    Want to join?
+                    <br />
+                    Enter your experience level and press Submit!
+                </p>
+                <br />
+                <JoinGroupForm />
+            </p>
+            <br />
+            <Link to={"/groups"} style={{fontWeight: 'bold'}}>Back to Groups List</Link>
         </div>
     )
 }
