5d71503221653778af35927f25f02a686e06a8d0
Badi-Haki Martin <jojackblack@gmail.com>
2023-01-06 12:52:14 -0500

    modified app/config/initializers/cors.rb to allow cross origin resource sharing requests. modified group messages to include a broadcast_message funct

--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -1,7 +1,15 @@
 class GroupMessage < ApplicationRecord
+    after_create_commit { broadcast_message }
+    
     # Associations
     belongs_to :user
     belongs_to :group
     has_many :replies, class_name: "GroupMessage", foreign_key: "comment_id"
     belongs_to :quoted_comment, class_name: "GroupMessage", optional: true
+
+    private
+
+    def broadcast_message
+        ActionCable.server.broadcast('public_chat',)
+    end
 end
