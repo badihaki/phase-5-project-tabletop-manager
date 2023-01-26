c14f204602c5ac6fc8dce367be6d17b089101449
Badi-Haki Martin <jojackblack@gmail.com>
2023-01-26 11:38:57 -0500

    front-end should be visible via render address. added lines to auto install front-end dependancies and build the app in bin/game... .sh. Front-end requests are handled through the api namespace in config/routes, so I need to update my front-end requests to include `api/...`

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  resources :characters
-  resources :group_messages, exclude: [:delete]
-  mount ActionCable.server => '/cable'
-  resources :memberships
-  resources :groups
-  post '/signup', to: 'users#create'
-  post '/login', to: 'sessions#create'
-  get '/me', to: 'users#show'
-  delete '/logout', to: 'sessions#destroy'
+  namespace :api do
+    resources :characters
+    resources :group_messages, exclude: [:delete]
+    mount ActionCable.server => '/cable'
+    resources :memberships
+    resources :groups
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    get '/me', to: 'users#show'
+    delete '/logout', to: 'sessions#destroy'
+  end
+
+    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
