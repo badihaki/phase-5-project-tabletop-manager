629c4c25a2c3604fcf759074673f827ec540a752
Badi-Haki Martin <jojackblack@gmail.com>
2023-02-05 17:44:48 -0500

    group page will no longer throw an error

--- client/src/components/GroupPage.js
+++ client/src/components/GroupPage.js
@@ -3,14 +3,14 @@ import { Link, useParams } from "react-router-dom";
 import { GroupsContext } from "./context components/GroupsContext";
 import { UserContext } from "./context components/UserContext";
 import MembershipForm from "./MembershipForm";
+import SignUpLogIn from "./SignUpLogIn";
 
 function GroupPage(){
     const { id } = useParams();
     const { groups } = useContext(GroupsContext);
     const { user } = useContext(UserContext);
 
-    const group = groups.find(group=>group.id == id);
-
+    
     function FormContainer(){
         return(
             <div>
@@ -24,41 +24,85 @@ function GroupPage(){
             </div>
         )
     }
+    
+    function GroupPageComponent(){
+        const group = groups.find( crew => crew.id == id );
+        return(
+            <div>
+                <h2>{group.name}</h2>
+                <h4>Game Master: {group.game_master.name}</h4>
+                <br />
+                <p>
+                    We play {group.game} on {group.game_day}.
+                    <br />
+                    <br />
+                </p>
+                    <h3>Members:</h3>
+                <ul>
+                    {group.players.map(player=>{
+                        const membership = group.memberships.find(m=>{
+                            return m.player_id === player.id;
+                        })
+                        return(
+                            <li key={player.id}>
+                                Player: <span style={{fontWeight:"bold", fontSize:"20px"}}>{player.name}</span>
+                                <br />
+                                Experience: <span style={{fontWeight:"bold"}}>{membership.player_experience_summary}</span>
+                                <br />
+                                <br />
+                            </li>
+                        )
+                    })}
+                </ul>
+                <br />
+                <br />
+                {user? <FormContainer /> : "" }
+                <br />
+                <Link to={"/groups"} style={{fontWeight: 'bold'}}>Back to Groups List</Link>
+            </div>
+        )
+    }
 
     return(
         <div>
-            <h2>{group.name}</h2>
-            <h4>Game Master: {group.game_master.name}</h4>
-            <br />
-            <p>
-                We play {group.game} on {group.game_day}.
-                <br />
-                <br />
-            </p>
-                <h3>Members:</h3>
-            <ul>
-                {group.players.map(player=>{
-                    const membership = group.memberships.find(m=>{
-                        return m.player_id == player.id;
-                    })
-                    return(
-                        <li key={player.id}>
-                            Player: <span style={{fontWeight:"bold", fontSize:"20px"}}>{player.name}</span>
-                            <br />
-                            Experience: <span style={{fontWeight:"bold"}}>{membership.player_experience_summary}</span>
-                            <br />
-                            <br />
-                        </li>
-                    )
-                })}
-            </ul>
-            <br />
-            <br />
-            {user? <FormContainer /> : "" }
-            <br />
-            <Link to={"/groups"} style={{fontWeight: 'bold'}}>Back to Groups List</Link>
+            {groups? <GroupPageComponent /> : <SignUpLogIn />}
         </div>
     )
+
+    // return(
+    //     <div>
+    //         <h2>{group.name}</h2>
+    //         <h4>Game Master: {group.game_master.name}</h4>
+    //         <br />
+    //         <p>
+    //             We play {group.game} on {group.game_day}.
+    //             <br />
+    //             <br />
+    //         </p>
+    //             <h3>Members:</h3>
+    //         <ul>
+    //             {group.players.map(player=>{
+    //                 const membership = group.memberships.find(m=>{
+    //                     return m.player_id == player.id;
+    //                 })
+    //                 return(
+    //                     <li key={player.id}>
+    //                         Player: <span style={{fontWeight:"bold", fontSize:"20px"}}>{player.name}</span>
+    //                         <br />
+    //                         Experience: <span style={{fontWeight:"bold"}}>{membership.player_experience_summary}</span>
+    //                         <br />
+    //                         <br />
+    //                     </li>
+    //                 )
+    //             })}
+    //         </ul>
+    //         <br />
+    //         <br />
+    //         {user? <FormContainer /> : "" }
+    //         <br />
+    //         <Link to={"/groups"} style={{fontWeight: 'bold'}}>Back to Groups List</Link>
+    //     </div>
+    // )
 }
 
 export default GroupPage;
